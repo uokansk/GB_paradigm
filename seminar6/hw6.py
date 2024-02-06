ce4ee5edbcb2ad321db1de3c9cf9d069cfd1315b
Aleksandr <uorkansk@yandex.ru>
2024-02-06 22:17:39 +0700

    решение задач 6 семинара

---
+++ seminar6/hw6.py
@@ -0,0 +1,27 @@
+"""
+Написать программу на любом языке в любой парадигме для
+бинарного поиска. На вход подаётся целочисленный массив и
+число. На выходе - индекс элемента или -1, в случае если искомого
+элемента нет в массиве.
+"""
+
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        mid_val = arr[mid]
+
+        if mid_val == target:
+            return mid
+        elif mid_val < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
+result = binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8)
+print(result)
